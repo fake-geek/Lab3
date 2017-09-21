87b753128deee643fbc2441377c62c6df3fe73a1
apple <apple@wirelessprv-10-192-154-152.near.illinois.edu>
2017-09-20 19:43:11 -0500

    V2 successful

--- src/main/java/Webscraper.java
+++ src/main/java/Webscraper.java
@@ -15,15 +15,41 @@ public class Webscraper {
     public static void main(final String[] args) {
         // TODO Auto-generated method stub
         Scanner urlScanner = new Scanner(System.in);
-        String nextLine = urlScanner.nextLine();
+        String uRL = urlScanner.nextLine();
         urlScanner.close();
 
-        String actualContents = urlToString(nextLine);
-        System.out.print(actualContents);
+        String actualContents = urlToString(uRL);
+        System.out.print(count(actualContents));
+    }
+
+
+
+    public static int count(String word) {
+        if (word == null || word.isEmpty()) {
+            return 0;
+        }
 
         int wordCount = 0;
 
+        boolean isWord = false;
+        int endOfLine = word.length() - 1;
+        char[] characters = word.toCharArray();
+
+        for (int i = 0; i < characters.length; i++) {
+            if (Character.isLetter(characters[i]) && i != endOfLine) {
+                isWord = true;
+            }else if (!Character.isLetter(characters[i]) && isWord) {
+                wordCount++;
+                isWord = false;
+            }else if (Character.isLetter(characters[i]) && i == endOfLine) {
+                wordCount++;
+            }
+        }
+        return wordCount;
     }
+
+
+
 
     /**
      * the method that convert URL to string.
